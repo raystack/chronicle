85e6a9a55a9deeb987741b41574338ec27d1d133
Rishabh <me@rsbh.dev>
2023-06-19 15:16:12 +0530

    chore: add schema parser

--- src/OpenApi/index.tsx
+++ src/OpenApi/index.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import jsYaml from 'js-yaml';
+import {OpenAPI, OpenAPIV3} from 'openapi-types'
+import styles from './openapi.module.scss'
+import SwaggerParser from "@apidevtools/swagger-parser";
+import humanizeString from 'humanize-string';
 
 interface OpenApiProps {
   schema: string;
@@ -6,5 +11,49 @@ interface OpenApiProps {
 }
 
 export function OpenApi({fileType = "json", schema}: OpenApiProps) {
-  return <div>Open Api</div>
+  const [parsedSchema, setParsedSchema] = useState<OpenAPI.Document | null>(null);
+
+  useEffect(() => {
+    const updateParsedSchema = async () => {
+      const schemaObj :OpenAPI.Document = fileType === "json" ? JSON.parse(schema) : jsYaml.load(schema);
+      const parsedSchemaObj = await SwaggerParser.parse(schemaObj);
+      setParsedSchema(parsedSchemaObj);
+    }
+    if (!parsedSchema) {
+      updateParsedSchema()
+    }
+  }, [parsedSchema, schema, fileType]);
+
+  const paths = useMemo(() => {
+    const paths = []
+    for (let path in parsedSchema?.paths) {
+      const methodsMap = parsedSchema?.paths[path]
+      for (let method in methodsMap) {
+        // @ts-ignore
+        const data = methodsMap[method] as OpenAPIV3.OperationObject
+        paths.push({
+          key:`${method}-${path}`,
+          path: path,
+          method: method,
+          operationData: data
+        })
+      }
+    }
+    return paths
+  }, [parsedSchema])
+
+  return <div className={styles.openapiWrapper}>
+    {
+      paths.map((pathData) => {
+        const title = humanizeString(pathData?.operationData?.operationId || "");
+        return <div className={styles.apiSection} key={pathData.key} id={pathData.operationData.operationId}>
+          <div>
+            <div className="title">{title}</div>
+            <div className="description">{pathData.operationData.description || pathData.operationData.summary}</div>
+          </div>
+         <div></div>
+      </div>
+      })
+    }
+  </div>
 }
