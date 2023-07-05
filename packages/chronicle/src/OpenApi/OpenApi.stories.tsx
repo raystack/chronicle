ca2e625d54dda9ef3a452faaf418e4ee4108e11d
Rishabh <me@rsbh.dev>
2023-07-05 15:27:03 +0530

    fix: storybook for openapi

--- packages/chronicle/src/OpenApi/OpenApi.stories.tsx
+++ packages/chronicle/src/OpenApi/OpenApi.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { OpenApi } from "./";
+import { parseSchema } from "../utils";
+import React from "react";
 
 // @ts-expect-error : sample data for import.
 import dataYamlv3 from "../../testData/sample-schema_v3.yaml?raw";
@@ -7,14 +9,16 @@ import dataYamlv3 from "../../testData/sample-schema_v3.yaml?raw";
 const meta: Meta<typeof OpenApi> = {
     title: "OpenApi",
     component: OpenApi,
+    render: (args, { loaded: { schema } }) => <OpenApi schema={schema} />,
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Yaml_v3: Story = {
-    args: {
-        schema: dataYamlv3,
-        fileType: "yaml",
-    },
+export const Primary: Story = {
+    loaders: [
+        async () => ({
+            schema: await parseSchema(dataYamlv3, "yaml"),
+        }),
+    ],
 };
