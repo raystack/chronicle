25cfebb4fb764fbd80bb6c46df13798abbd46ea4
Rishabh <me@rsbh.dev>
2026-02-04 12:37:39 +0530

    chore: remove header

--- packages/chronicle/src/themes/default/Page.tsx
+++ packages/chronicle/src/themes/default/Page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Flex, Headline, Text } from '@raystack/apsara'
+import { Flex } from '@raystack/apsara'
 import type { ThemePageProps } from '../../types'
 import { Toc } from './Toc'
 import styles from './Page.module.css'
@@ -9,16 +9,6 @@ export function Page({ page }: ThemePageProps) {
   return (
     <Flex className={styles.page}>
       <article className={styles.article}>
-        <header className={styles.header}>
-          <Headline as="h1" size="large">
-            {page.frontmatter.title}
-          </Headline>
-          {page.frontmatter.description && (
-            <Text size={4} className={styles.description}>
-              {page.frontmatter.description}
-            </Text>
-          )}
-        </header>
         <div className={styles.content}>
           {page.content}
         </div>
